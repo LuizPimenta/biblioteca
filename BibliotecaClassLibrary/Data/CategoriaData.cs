f8b96965dafc1825e2380a439b3d5a5ebb0db60b
LuizPimenta <hick_sth@hotmail.com>
2015-02-27 12:49:42 -0300

    Criação dos Datas 27/02

---
+++ BibliotecaClassLibrary/Data/CategoriaData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using BibliotecaClassLibrary.Entity;
+
+
+namespace BibliotecaClassLibrary.Data
+{
+    internal class CategoriaData:DBConnection
+    {
+        internal CategoriaData(string strConn) : base(strConn) { }
+        internal void Create(Categoria e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "insert into Categoria values (@nome)";
+            cmd.Parameters.AddWithValue("@nome", e.Nome);
+            cmd.ExecuteNonQuery();
+        }
+
+        internal void update(Categoria e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "Update Categoria set nome = @nome where id = @id";
+            cmd.Parameters.AddWithValue("@id", e.IdCategoria);
+            cmd.Parameters.AddWithValue("@Nome", e.Nome);
+            cmd.ExecuteNonQuery();       
+        }
+
+    }
+}
---
+++ BibliotecaConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaClassLibrary.Entity;
+
+namespace BibliotecaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Categoria objCategoria = new Categoria();
+
+            Console.WriteLine("Nome da Categoria: ");
+            objCategoria.Nome = Console.ReadLine();
+
+            Console.WriteLine("Categoria:" + objCategoria.Nome);
+
+
+
+            Usuario objUsuario = new Usuario();
+
+            Console.WriteLine("Digite o nome do Usuario: ");
+            objUsuario.Nome = Console.ReadLine();
+
+            Console.WriteLine("Usuario:" + objUsuario.Nome);
+
+        }
+    }
+}
